f36ebf8ca34f83d3402811b9761859c53f816d91
Rodrigo Alves <rodrigovieira1994@gmail.com>
2015-02-23 06:57:34 -0300

    Hell yeah Carousel is working

---
+++ Classboard-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Classboard-Bridging-Header.h
+//  Classboard
+//
+//  Created by Rodrigo Alves on 2/23/15.
+//  Copyright (c) 2015 Sgt. Peppers. All rights reserved.
+//
+
+#ifndef Classboard_Classboard_Bridging_Header_h
+#define Classboard_Classboard_Bridging_Header_h
+
+#import "iCarousel.h"
+
+#endif
